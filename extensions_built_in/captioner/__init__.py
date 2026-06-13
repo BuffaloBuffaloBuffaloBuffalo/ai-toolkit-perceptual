9dc8946774d26722bba48ab1a8b2891a85fff38b
JLB <>
2026-06-13 14:13:30 -0500

    fix(ideogram4): drop AceStep from captioner registry
    Companion to AceStepCaptioner.py removal; registry no longer references it.

--- extensions_built_in/captioner/__init__.py
+++ extensions_built_in/captioner/__init__.py
@@ -1,18 +1,6 @@
 from toolkit.extension import Extension
 
 
-class AceStepCaptionerExtension(Extension):
-    uid = "AceStepCaptioner"
-    name = "Ace Step Captioner"
-
-    @classmethod
-    def get_process(cls):
-        # import your process class here so it is only loaded when needed and return it
-        from .AceStepCaptioner import AceStepCaptioner
-
-        return AceStepCaptioner
-
-
 class Qwen3VLCaptionerExtension(Extension):
     uid = "Qwen3VLCaptioner"
     name = "Qwen 3VL Captioner"
@@ -38,7 +26,6 @@ class Ideogram4CaptionerExtension(Extension):
 
 
 AI_TOOLKIT_EXTENSIONS = [
-    AceStepCaptionerExtension,
     Qwen3VLCaptionerExtension,
     Ideogram4CaptionerExtension,
 ]
